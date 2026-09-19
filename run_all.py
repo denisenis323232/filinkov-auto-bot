aaa49e1f8c8195fe20fd700f0b9ef3d898ebf9d6
denisenis323232 <denisfilinkov1999@gmail.com>
2026-09-19 16:52:02 +0300

    Run safe photo processor v3 and bot hotfix

--- run_all.py
+++ run_all.py
@@ -8,8 +8,8 @@ import time
 
 
 def main() -> int:
-    processor = subprocess.Popen([sys.executable, "run_photo_processor_v2.py"], env=os.environ.copy())
-    bot = subprocess.Popen([sys.executable, "run_progress.py"], env=os.environ.copy())
+    processor = subprocess.Popen([sys.executable, "run_photo_processor_v3.py"], env=os.environ.copy())
+    bot = subprocess.Popen([sys.executable, "run_hotfix.py"], env=os.environ.copy())
 
     def stop(_sig=None, _frame=None):
         for p in (processor, bot):
